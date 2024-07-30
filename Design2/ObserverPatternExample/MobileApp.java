92c2ac8bbf23eebb4b8f628de0b4ac4f527d6811
Rupsa1037 <165894906+Rupsa1037@users.noreply.github.com>
2024-07-30 17:38:01 +0530

    Delete Design2 directory

--- Design2/ObserverPatternExample/MobileApp.java
+++
@@ -1,7 +0,0 @@
-public class MobileApp implements Observer
-{
-    public void update(double amount)
-    {
-         System.out.println("Mobile App: Received update for stock market= "+amount);
-    }
-}
--- Design2/ObserverPatternExample/WebApp.java
+++
@@ -1,7 +0,0 @@
-public class WebApp implements Observer
-{
-    public void update(double amount)
-    {
-         System.out.println("Web App: Received update for stock market= "+amount);
-    }
-}
