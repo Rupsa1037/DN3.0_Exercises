46fdb8319f6c296e978f426170c8f48dd8ac6b94
Rupsa1037 <rupsanandy78@gmail.com>
2024-08-22 22:12:43 +0530

    commit

---
+++ Exercise 1/bookstoreapi/src/test/java/com/code/api/bookstoreapi/BookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.api.bookstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
